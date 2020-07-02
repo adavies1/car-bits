ea71ecc916251c6fa273fd1c1ec3f7cc91e78ce2
Alex Davies <alexdavieswebdev@gmail.com>
2020-07-02 16:32:05 +0100

    - Updated header, added logo and responsive styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <>
-      <Header title="Checkout"/>
+      <Header title="CarBits.co.uk" subtitle="Helping keep sheds and bangers on the road since 1994"/>
       <main className="container">
         <div className="grid grid--checkout">
           <p>main content</p>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,19 @@ import css from './Header.module.scss';
 
 type HeaderProps = {
     title: string
+    subtitle?: string
 };
 
 const header:FunctionComponent<HeaderProps> = props => (
     <header className={css.header}>
         <div className="container">
-            <h1 className={css.title}>{props.title}</h1>
+            <div className={css.top}>
+                <img className={css.logo} src="/img/allegro.png" alt="Austin Allegro"/>
+                <div>
+                    <h1 className={css.title}>{props.title}</h1>
+                    {props.subtitle && <p className={css.subtitle}>{props.subtitle}</p>}
+                </div>
+            </div>
         </div>
         <nav className={css.nav}></nav>
     </header>
